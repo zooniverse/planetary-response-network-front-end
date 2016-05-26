f952500cada074acce99d13b9d016dac5f442358
Tom Jenkins <tom@itsravenous.com>
2016-05-26 16:19:11 +0100

    Hide nav items when not logged in

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -16,30 +16,39 @@ export default class Header extends React.Component {
       .then(user => this.setState({user}));
   }
 
+  renderNav() {
+    const mainNavDisplay = this.state.user ? (
+      <ul className="nav navbar-nav">
+        <li><Link to="/upload">Upload</Link></li>
+        <li><Link to="/jobs">Jobs</Link></li>
+        <li><Link to="/settings">Settings</Link></li>
+      </ul>
+    ) : null;
+    return (
+      <div id="navbar-collapse" className="collapse navbar-collapse">
+        {mainNavDisplay}
+        <ul className="nav navbar-nav navbar-right">
+          <li>
+            {this.state.user ?
+              <LoggedInUser user={this.state.user} logout={auth.logout.bind(auth)}/> :
+              <LoginButton login={auth.login.bind(auth)}/>}
+          </li>
+        </ul>
+      </div>
+    );
+  }
+
   render() {
     console.log('this.state.user = ', this.state.user);
     return (
       <nav className="navbar navbar-default navbar-static-top">
         <div className="container">
           <div className="navbar-header">
+            {this.renderNav()}
             <button type="button" data-toggle="collapse" data-target="#navbar-collapse" className="navbar-toggle collapsed">
               <span className="sr-only">Toggle Navigation</span>
               <span className="icon-bar"></span><i className="fa fa-bars fa-2x"/><span className="icon-bar"></span>
             </button>
-          </div>
-          <div id="navbar-collapse" className="collapse navbar-collapse">
-            <ul className="nav navbar-nav">
-              <li><Link to="/upload">Upload</Link></li>
-              <li><Link to="/jobs">Jobs</Link></li>
-              <li><Link to="/settings">Settings</Link></li>
-            </ul>
-            <ul className="nav navbar-nav navbar-right">
-              <li>
-                {this.state.user ?
-                  <LoggedInUser user={this.state.user} logout={auth.logout.bind(auth)}/> :
-                  <LoginButton login={auth.login.bind(auth)}/>}
-              </li>
-            </ul>
           </div>
         </div>
       </nav>
